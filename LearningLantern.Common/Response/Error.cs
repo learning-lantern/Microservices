40c3c4a8a6c90e346bc3a8f11a6cb9e448125a89
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-18 21:15:40 +0200

    Remove JsonIgnonre on Status Code And Create ValidationFailure.cs

--- LearningLantern.Common/Response/Error.cs
+++ LearningLantern.Common/Response/Error.cs
@@ -1,12 +1,9 @@
-using System.Text.Json.Serialization;
-using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 
 namespace LearningLantern.Common.Response;
 
 public abstract class ErrorBase
 {
-    [JsonIgnore]
     public int StatusCode { get; init; }
 }
 
@@ -27,9 +24,4 @@ public class ValidationError : ErrorBase
 
     public string PropertyName { get; init; }
     public List<string> ErrorMessages { get; init; }
-}
-
-public static class ValidationFailureExtensions
-{
-    public static IEnumerable<ValidationError> ToApplicationValidationError(this IEnumerable<ValidationFailure> failure) => failure.GroupBy(x => x.ErrorCode, x => x.ErrorMessage).Select(x => new ValidationError(x.Key, x.ToList()));
 }
