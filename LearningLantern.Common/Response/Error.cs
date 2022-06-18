54d2b6e15cfe95dd61d7e7684ba8f465a6223427
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-18 20:22:36 +0200

    Improve Error.cs

--- LearningLantern.Common/Response/Error.cs
+++ LearningLantern.Common/Response/Error.cs
@@ -1,24 +1,27 @@
+using System.Text.Json.Serialization;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
 
 namespace LearningLantern.Common.Response;
 
 public abstract class ErrorBase
 {
-    [JsonIgnore] public int StatusCode { get; init; }
+    [JsonIgnore]
+    public int StatusCode { get; init; }
 }
 
 public class Error : ErrorBase
 {
-    public string Description { get; init; }
-    public string ErrorCode { get; init; }
+    public string Description { get; init; } = null!;
+    public string ErrorCode { get; init; } = null!;
 }
 
 public class ValidationError : ErrorBase
 {
-    public ValidationError()
+    public ValidationError(string propertyName, List<string> errorMessages)
     {
+        PropertyName = propertyName;
+        ErrorMessages = errorMessages;
         StatusCode = StatusCodes.Status400BadRequest;
     }
 
@@ -28,14 +31,5 @@ public class ValidationError : ErrorBase
 
 public static class ValidationFailureExtensions
 {
-    public static IEnumerable<ValidationError> ToApplicationValidationError(this IEnumerable<ValidationFailure> failure)
-    {
-        return failure.GroupBy(x => x.ErrorCode, x => x.ErrorMessage).Select(x =>
-            new ValidationError
-            {
-                PropertyName = x.Key,
-                ErrorMessages = x.ToList()
-            }
-        );
-    }
+    public static IEnumerable<ValidationError> ToApplicationValidationError(this IEnumerable<ValidationFailure> failure) => failure.GroupBy(x => x.ErrorCode, x => x.ErrorMessage).Select(x => new ValidationError(x.Key, x.ToList()));
 }
