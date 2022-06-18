1be34c0c915666acf1844d1aa92f7978a55b113c
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-18 21:17:28 +0200

    Create ValidationFailure.cs

---
+++ LearningLantern.Common/Extensions/ValidationFailure.cs
@@ -0,0 +1,9 @@
+using FluentValidation.Results;
+using LearningLantern.Common.Response;
+
+namespace LearningLantern.Common.Extensions;
+
+public static class ValidationFailureExtensions
+{
+    public static IEnumerable<ValidationError> ToApplicationValidationError(this IEnumerable<ValidationFailure> failure) => failure.GroupBy(x => x.ErrorCode, x => x.ErrorMessage).Select(x => new ValidationError(x.Key, x.ToList()));
+}
