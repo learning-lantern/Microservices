5fa76681f1169bdd5a5271d3a3228af46f624153
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-18 22:35:29 +0200

    Review LearningLantern.Common

--- LearningLantern.Common/Services/EmailSender.cs
+++ LearningLantern.Common/Services/EmailSender.cs
@@ -1,18 +1,19 @@
 using System.Net;
 using System.Net.Mail;
-using LearningLantern.Common.Response;
+using LearningLantern.Common.Responses;
 
 namespace LearningLantern.Common.Services;
 
 public class EmailSender : IEmailSender
 {
-    private readonly SmtpClient _client;
-    private readonly string _fromEmail;
+    private readonly SmtpClient client;
+    private readonly string fromEmail;
 
     public EmailSender(string email, string password, string smtpServerAddress, int mailPort)
     {
-        _fromEmail = email;
-        _client = new SmtpClient
+        fromEmail = email;
+
+        client = new SmtpClient
         {
             Host = smtpServerAddress,
             Port = mailPort,
@@ -21,11 +22,11 @@ public class EmailSender : IEmailSender
         };
     }
 
-    public async Task<Response.Response> Send(string emailTo, Message message)
+    public async Task<Response> Send(string toEmail, Message message)
     {
         try
         {
-            await Send(emailTo, message.EmailSubject, message.EmailBody, message.IsBodyHtml);
+            await Send(toEmail, message.EmailSubject, message.EmailBody, message.IsBodyHtml);
             return ResponseFactory.Ok();
         }
         catch (Exception ex)
@@ -40,13 +41,13 @@ public class EmailSender : IEmailSender
         }
     }
 
-    private Task Send(string emailTo, string emailSubject, string emailBody, bool isBodyHtml = true)
+    private async Task Send(string emailTo, string emailSubject, string emailBody, bool isBodyHtml = true)
     {
-        var mailMessage = new MailMessage(_fromEmail, emailTo, emailSubject, emailBody)
+        var mailMessage = new MailMessage(fromEmail, emailTo, emailSubject, emailBody)
         {
             IsBodyHtml = isBodyHtml
         };
 
-        return _client.SendMailAsync(mailMessage);
+        await client.SendMailAsync(mailMessage);
     }
 }
--- LearningLantern.Common/Services/IEmailSender.cs
+++ LearningLantern.Common/Services/IEmailSender.cs
@@ -1,6 +1,8 @@
+using LearningLantern.Common.Responses;
+
 namespace LearningLantern.Common.Services;
 
 public interface IEmailSender
 {
-    public Task<Response.Response> Send(string emailTo, Message message);
+    public Task<Response> Send(string toEmail, Message message);
 }
