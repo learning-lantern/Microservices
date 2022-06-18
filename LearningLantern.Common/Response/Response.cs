83596ac4d4817ba475af3472a520c3270b9d9ab6
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-18 21:05:05 +0200

    Add T? data optional argument for Fail function

--- LearningLantern.Common/Response/Response.cs
+++ LearningLantern.Common/Response/Response.cs
@@ -6,20 +6,24 @@ public static class ResponseFactory
     public static Response<T> Ok<T>(T? data = default) => new(true, data, default);
 
     public static Response Fail() => new(false, default);
-    public static Response Fail(ErrorBase error) => new(false, new[] {error});
+    public static Response Fail(ErrorBase error) => new(false, new[] { error });
     public static Response Fail(IEnumerable<ErrorBase> errors) => new(false, errors);
-    public static Response<T> Fail<T>() => new(false, default, default);
-    public static Response<T> Fail<T>(ErrorBase error, T? data = default) => Fail(new[] {error}, data);
+    public static Response<T> Fail<T>(T? data = default) => new(false, data, default);
+    public static Response<T> Fail<T>(ErrorBase error, T? data = default) => Fail(new[] { error }, data);
     public static Response<T> Fail<T>(IEnumerable<ErrorBase> errors, T? data = default) => new(false, data, errors);
 
-    public static TResponse? CreateFailObject<TResponse>(IEnumerable<ErrorBase> failures)
-        where TResponse : Response
-
+    public static TResponse? CreateFailObject<TResponse>(IEnumerable<ErrorBase> failures) where TResponse : Response
     {
         var responseType = typeof(TResponse);
-        if (!responseType.IsGenericType) return Activator.CreateInstance(responseType, false, failures) as TResponse;
+
+        if (!responseType.IsGenericType)
+        {
+            return Activator.CreateInstance(responseType, false, failures) as TResponse;
+        }
+
         var resultType = responseType.GetGenericArguments()[0];
         var invalidResponseType = typeof(Response).MakeGenericType(resultType);
+
         return Activator.CreateInstance(invalidResponseType, false, null, failures) as TResponse;
     }
 }
@@ -38,8 +42,7 @@ public class Response
 
 public class Response<T> : Response
 {
-    public Response(bool succeeded, T? data, IEnumerable<ErrorBase>? errors)
-        : base(succeeded, errors)
+    public Response(bool succeeded, T? data, IEnumerable<ErrorBase>? errors) : base(succeeded, errors)
     {
         Data = data;
     }
