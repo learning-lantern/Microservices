381f544b977cccfe9b242aad9826c7b091f23dd5
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-22 07:30:28 +0200

    Update ApiGateway.LearningLanternContext.cs and remove unnessary code

--- ApiGateway/LearningLantern.ApiGateway/Data/LearningLanternContext.cs
+++ ApiGateway/LearningLantern.ApiGateway/Data/LearningLanternContext.cs
@@ -10,30 +10,12 @@ namespace LearningLantern.ApiGateway.Data;
 /// </summary>
 public class LearningLanternContext : IdentityDbContext<UserModel>, ILearningLanternContext
 {
-    public LearningLanternContext(DbContextOptions option) : base(option)
-    {
-    }
+    public LearningLanternContext(DbContextOptions option) : base(option) { }
 
     public DbSet<ClassroomModel> Classrooms { get; set; } = null!;
-    public DbSet<ClassroomUserModel> ClassroomUsers { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-        builder.Entity<ClassroomUserModel>()
-            .HasKey(classroomUserModel => new {classroomUserModel.ClassroomId, classroomUserModel.UserId});
-
-        builder.Entity<ClassroomUserModel>()
-            .HasOne(classroomUserModel => classroomUserModel.User)
-            .WithMany(userModel => userModel.ClassroomUsers)
-            .HasForeignKey(classroomUserModel => classroomUserModel.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        builder.Entity<ClassroomUserModel>()
-            .HasOne(classroomUserModel => classroomUserModel.Classroom)
-            .WithMany(classroomModel => classroomModel.ClassroomUsers)
-            .HasForeignKey(classroomUserModel => classroomUserModel.ClassroomId)
-            .OnDelete(DeleteBehavior.Cascade);
     }
 }
