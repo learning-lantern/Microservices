cf7f4849359912ea4ab3ec59de9b0b2351f5b124
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-22 07:29:14 +0200

    Update ApiGateway.ILearningLanternContext.cs

--- ApiGateway/LearningLantern.ApiGateway/Data/ILearningLanternContext.cs
+++ ApiGateway/LearningLantern.ApiGateway/Data/ILearningLanternContext.cs
@@ -6,7 +6,6 @@ namespace LearningLantern.ApiGateway.Data;
 public interface ILearningLanternContext
 {
     DbSet<ClassroomModel> Classrooms { get; set; }
-    DbSet<ClassroomUserModel> ClassroomUsers { get; set; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
