b752fcf71731e424ab7b957a4758ecd3b8804836
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-22 07:37:28 +0200

    Update ErrorsList class

---
+++ ApiGateway/LearningLantern.ApiGateway/Utility/ErrorsList.cs
@@ -0,0 +1,55 @@
+using LearningLantern.Common.Responses;
+
+namespace LearningLantern.ApiGateway.Utility;
+
+public static class ErrorsList
+{
+    public static Error UserEmailNotFound(string email) => new()
+    {
+        StatusCode = StatusCodes.Status404NotFound,
+        ErrorCode = nameof(UserEmailNotFound),
+        Description = $"There is no user in this University with this email {email}."
+    };
+
+    public static Error UserIdNotFound(string userId) => new()
+    {
+        StatusCode = StatusCodes.Status404NotFound,
+        ErrorCode = nameof(UserIdNotFound),
+        Description = $"There is no user in this University with this Id {userId}."
+    };
+
+    public static Error IncorrectPassword(string userId) => new()
+    {
+        StatusCode = StatusCodes.Status403Forbidden,
+        ErrorCode = nameof(IncorrectPassword),
+        Description = $"This is incorrect password for this user {userId}"
+    };
+
+    public static Error SignInNotAllowed() => new()
+    {
+        StatusCode = StatusCodes.Status400BadRequest,
+        ErrorCode = nameof(SignInNotAllowed),
+        Description = "SignIn not allowed"
+    };
+
+    public static Error SignInFailed() => new()
+    {
+        StatusCode = StatusCodes.Status400BadRequest,
+        ErrorCode = nameof(SignInFailed),
+        Description = "SignIn Failed"
+    };
+
+    public static Error NameNotValid() => new()
+    {
+        StatusCode = StatusCodes.Status400BadRequest,
+        ErrorCode = nameof(NameNotValid),
+        Description = "This name is not valid, the length of the alphabetic characters must be greater than or equal to 2."
+    };
+
+    public static Error ClassroomIdNotFound(int classroomId) => new()
+    {
+        StatusCode = StatusCodes.Status404NotFound,
+        ErrorCode = nameof(ClassroomIdNotFound),
+        Description = $"Classroom {classroomId} is not Found"
+    };
+}
