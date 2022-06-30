785c7c526985bffc549706d89765b5b7bb5d9c74
ahmedezzatg <ahmed.ezzat.ga@gmail.com>
2022-06-30 20:27:52 +0200

    Update ConfirmEmailCommand

--- ApiGateway/LearningLantern.ApiGateway/Users/Commands/ConfirmEmailCommand.cs
+++ ApiGateway/LearningLantern.ApiGateway/Users/Commands/ConfirmEmailCommand.cs
@@ -1,5 +1,6 @@
 using System.Web;
 using AutoMapper;
+using LearningLantern.ApiGateway.Data.DTOs;
 using LearningLantern.ApiGateway.Data.Models;
 using LearningLantern.ApiGateway.Users.Events;
 using LearningLantern.ApiGateway.Utility;
@@ -10,41 +11,45 @@ using Microsoft.AspNetCore.Identity;
 
 namespace LearningLantern.ApiGateway.Users.Commands;
 
-public class ConfirmEmailCommand : IRequest<Response>
+public class ConfirmEmailCommand : IRequest<Response<TokenResponseDTO>>
 {
     public string UserId { get; set; } = null!;
     public string Token { get; set; } = null!;
 }
 
-public class ConfirmEmailHandler : IRequestHandler<ConfirmEmailCommand, Response>
+public class ConfirmEmailHandler : IRequestHandler<ConfirmEmailCommand, Response<TokenResponseDTO>>
 {
     private readonly IEventBus _eventBus;
+    private readonly JWTGenerator _jwtGenerator;
+    private readonly ILogger<ConfirmEmailHandler> _logger;
     private readonly IMapper _mapper;
     private readonly UserManager<UserModel> _userManager;
-    private readonly ILogger<ConfirmEmailHandler> _logger;
 
-    public ConfirmEmailHandler(IEventBus eventBus, IMapper mapper, UserManager<UserModel> userManager, ILogger<ConfirmEmailHandler> logger)
+    public ConfirmEmailHandler(
+        IEventBus eventBus, IMapper mapper, UserManager<UserModel> userManager, ILogger<ConfirmEmailHandler> logger,
+        JWTGenerator jwtGenerator)
     {
         _eventBus = eventBus;
         _mapper = mapper;
         _userManager = userManager;
         _logger = logger;
+        _jwtGenerator = jwtGenerator;
     }
 
-    public async Task<Response> Handle(ConfirmEmailCommand request, CancellationToken cancellationToken)
+    public async Task<Response<TokenResponseDTO>> Handle(ConfirmEmailCommand request, CancellationToken cancellationToken)
     {
         var user = await _userManager.FindByIdAsync(request.UserId);
-        if (user is null) return ResponseFactory.Fail(ErrorsList.UserIdNotFound(request.UserId));
+        if (user is null) return ResponseFactory.Fail<TokenResponseDTO>(ErrorsList.UserIdNotFound(request.UserId));
 
         var token = HttpUtility.UrlDecode(request.Token);
-        
+
         _logger.LogInformation($"requst token = {request.Token}");
         _logger.LogInformation($"token = {token}");
 
         var result = await _userManager.ConfirmEmailAsync(user, request.Token);
 
-        if (result.Succeeded) _eventBus.Publish(_mapper.Map<UpdateUserEvent>(user));
-
-        return result.ToApplicationResponse();
+        if (result.Succeeded == false) return result.ToApplicationResponse<TokenResponseDTO>();
+        _eventBus.Publish(_mapper.Map<UpdateUserEvent>(user));
+        return ResponseFactory.Ok(await _jwtGenerator.GenerateJwtSecurityToken(user));
     }
 }
--- ApiGateway/LearningLantern.ApiGateway/Users/Commands/SendConfirmationEmailCommand.cs
+++ ApiGateway/LearningLantern.ApiGateway/Users/Commands/SendConfirmationEmailCommand.cs
@@ -14,24 +14,21 @@ public class SendConfirmationEmailCommand : AuthorizedRequest<Response<TokenResp
 {
 }
 
-public class SendConfirmationEmailCommandHandler : IRequestHandler<SendConfirmationEmailCommand, Response<TokenResponseDTO>>
+public class SendConfirmationEmailCommandHandler : IRequestHandler<SendConfirmationEmailCommand, Response>
 {
     private readonly IEmailSender _emailSender;
-    private readonly JWTGenerator _jwtGenerator;
     private readonly ILogger<SendConfirmationEmailCommandHandler> _logger;
     private readonly UserManager<UserModel> _userManager;
 
     public SendConfirmationEmailCommandHandler(
-        UserManager<UserModel> userManager, IEmailSender emailSender, ILogger<SendConfirmationEmailCommandHandler> logger,
-        JWTGenerator jwtGenerator)
+        UserManager<UserModel> userManager, IEmailSender emailSender, ILogger<SendConfirmationEmailCommandHandler> logger)
     {
         _userManager = userManager;
         _emailSender = emailSender;
         _logger = logger;
-        _jwtGenerator = jwtGenerator;
     }
 
-    public async Task<Response<TokenResponseDTO>> Handle(
+    public async Task<Response> Handle(
         SendConfirmationEmailCommand request, CancellationToken cancellationToken)
     {
         var token = await _userManager.GenerateEmailConfirmationTokenAsync(request.User);
@@ -40,10 +37,6 @@ public class SendConfirmationEmailCommandHandler : IRequestHandler<SendConfirmat
         _logger.LogInformation($"confirm Email encodingToken {encodingToken}");
         var mailMessage = MessageTemplates.ConfirmationEmail(request.User.Id, encodingToken);
 
-        var response = await _emailSender.Send(request.User.Email, mailMessage);
-
-        return response.Succeeded
-            ? ResponseFactory.Ok(await _jwtGenerator.GenerateJwtSecurityToken(request.User))
-            : ResponseFactory.Fail<TokenResponseDTO>(response.Errors!);
+        return await _emailSender.Send(request.User.Email, mailMessage);
     }
 }
