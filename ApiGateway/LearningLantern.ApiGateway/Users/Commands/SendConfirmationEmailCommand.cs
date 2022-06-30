c2db539081db78832a3f4f3aabceb32c3416ae49
ahmedezzatg <ahmed.ezzat.ga@gmail.com>
2022-06-30 18:47:52 +0200

    update SendConfirmationEmailCommand.cs

--- ApiGateway/LearningLantern.ApiGateway/Users/Commands/SendConfirmationEmailCommand.cs
+++ ApiGateway/LearningLantern.ApiGateway/Users/Commands/SendConfirmationEmailCommand.cs
@@ -1,4 +1,3 @@
-using System.Web;
 using LearningLantern.ApiGateway.Data.Models;
 using LearningLantern.ApiGateway.Users.BuildingBlocks;
 using LearningLantern.ApiGateway.Utility;
@@ -16,19 +15,22 @@ public class SendConfirmationEmailCommand : AuthorizedRequest<Response>
 public class SendConfirmationEmailCommandHandler : IRequestHandler<SendConfirmationEmailCommand, Response>
 {
     private readonly IEmailSender _emailSender;
+    private readonly ILogger<SendConfirmationEmailCommandHandler> _logger;
     private readonly UserManager<UserModel> _userManager;
 
-
-    public SendConfirmationEmailCommandHandler(UserManager<UserModel> userManager, IEmailSender emailSender)
+    public SendConfirmationEmailCommandHandler(
+        UserManager<UserModel> userManager, IEmailSender emailSender, ILogger<SendConfirmationEmailCommandHandler> logger)
     {
         _userManager = userManager;
         _emailSender = emailSender;
+        _logger = logger;
     }
 
     public async Task<Response> Handle(SendConfirmationEmailCommand request, CancellationToken cancellationToken)
     {
         var token = await _userManager.GenerateEmailConfirmationTokenAsync(request.User);
 
+        _logger.LogInformation($"confirm Email token {token}");
         var mailMessage = MessageTemplates.ConfirmationEmail(request.User.Id, token);
 
         return await _emailSender.Send(request.User.Email, mailMessage);
