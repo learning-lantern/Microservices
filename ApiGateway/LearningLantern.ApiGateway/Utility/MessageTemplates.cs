51ea2fd4f61ee44c7fba48c35f34d7d00162bf85
ahmedezzatg <ahmed.ezzat.ga@gmail.com>
2022-06-30 18:04:17 +0200

    change ConfirmationEmailEndpoint in email

--- ApiGateway/LearningLantern.ApiGateway/Utility/MessageTemplates.cs
+++ ApiGateway/LearningLantern.ApiGateway/Utility/MessageTemplates.cs
@@ -4,7 +4,7 @@ namespace LearningLantern.ApiGateway.Utility;
 
 public static class MessageTemplates
 {
-    private const string ConfirmationEmailEndpoint = "";
+    private const string ConfirmationEmailEndpoint = "https://learning-lantern.web.app/auth/validate-email";
 
     public static Message ConfirmationEmail(string userId, string token)
     {
