7a0dd7949e494a15d779f8f5726cfea0026b5c77
Omnia1Ahmed <omnia259ahmed@gmail.com>
2022-07-01 22:07:16 +0200

    Update

--- Video/LearningLantern.Video/Data/Models/AddVideoDTO.cs
+++ Video/LearningLantern.Video/Data/Models/AddVideoDTO.cs
@@ -7,15 +7,11 @@ public class AddVideoDTO : VideoProperties
     [Required]
     public string Path { get; set; } = null!;
 
-    [Required]
-    public string Type { get; set; } = null!;
-
-
+    
     public AddVideoDTO() { }
 
     public AddVideoDTO(AddVideoDTO video) : base(video)
     {
         Path = video.Path;
-        Type = video.Type;
     }
 }
