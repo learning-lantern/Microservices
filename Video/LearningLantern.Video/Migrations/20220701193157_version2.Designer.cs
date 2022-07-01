4bfd305cb8dd5bdd2e64d764e2f897a78e9f9982
ahmedezzatg <ahmed.ezzat.ga@gmail.com>
2022-07-01 21:34:11 +0200

    meagration

--- Video/LearningLantern.Video/Migrations/20220701193157_version2.Designer.cs
+++ Video/LearningLantern.Video/Migrations/20220701193157_version2.Designer.cs
@@ -3,6 +3,7 @@ using LearningLantern.Video.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LearningLantern.Video.Migrations
 {
     [DbContext(typeof(VideoContext))]
-    partial class VideoContextModelSnapshot : ModelSnapshot
+    [Migration("20220701193157_version2")]
+    partial class version2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
