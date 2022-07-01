47b3e58d972de1fe0921df80c08e472e670699e4
ahmedezzatg <ahmed.ezzat.ga@gmail.com>
2022-07-02 00:07:23 +0200

    Update MyDay prop

--- TodoList/LearningLantern.TodoList/Data/Models/TaskProperties.cs
+++ TodoList/LearningLantern.TodoList/Data/Models/TaskProperties.cs
@@ -6,9 +6,9 @@ public class TaskProperties
 {
     [Required] [StringLength(450)] public string Title { get; set; } = null!;
     public string? Description { get; set; }
-    public DateTime? StartDate { get; set; }
-    public DateTime? DueDate { get; set; }
-    public bool MyDay => StartDate <= DateTime.UtcNow && DateTime.UtcNow <= DueDate;
+    public DateTime StartDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool MyDay => StartDate.Date <= DateTime.Today && DateTime.Today <= DueDate.Date;
     public bool Completed { get; set; }
     public bool Important { get; set; }
 }
