e7c14e770591920533a721eff328379f2f31b5dd
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-07-02 15:35:33 +0200

    Update Video

--- Libraries/LearningLantern.BlobStorage/DependencyInjection.cs
+++ Libraries/LearningLantern.BlobStorage/DependencyInjection.cs
@@ -5,8 +5,10 @@ namespace LearningLantern.AzureBlobStorage;
 
 public static class DependencyInjection
 {
+    private static readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=learninglantern;AccountKey=WKgvAxchhiCJw/TnI8DqzEuuEITQ7DAiQzjrWwsqytsdaRKFBd4VSI6JI9Rz3Jxbtj6LVbhoA9Qd+AStd18WWA==;EndpointSuffix=core.windows.net";
+    
     public static IServiceCollection AddBlobService(
-        this IServiceCollection services, string connectionString, string containerName)
+        this IServiceCollection services, string containerName)
     {
         services.AddSingleton<IBlobService, BlobService>(
             provider => new BlobService(connectionString, containerName, provider.GetService<ILogger<BlobService>>())
