2f22524ba22f9d0e70e86072f2b773b206578a44
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-07-04 22:15:06 +0200

    Update TextLesson

--- TextLesson/LearningLantern.TextLesson/Data/Models/TextLessonModel.cs
+++ TextLesson/LearningLantern.TextLesson/Data/Models/TextLessonModel.cs
@@ -6,7 +6,7 @@ namespace LearningLantern.TextLesson.Data.Models;
 public class TextLessonModel
 {
     [Key] [Required] public int Id { get; set; }
-    [Required] public string ClassroomId { get; set; }
+    [Required] public string ClassroomId { get; set; } = null!;
     [Required] public string Title { get; set; } = null!;
     [Required] public string BlobName { get; set; } = null!;
 }
