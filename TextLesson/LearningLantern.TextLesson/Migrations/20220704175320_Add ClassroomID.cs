8aeebefc58ec1db46fa1c1b72c95e2855930e1ce
ahmedezzatg <ahmed.ezzat.ga@gmail.com>
2022-07-04 19:57:31 +0200

    Update TextLesson database

---
+++ TextLesson/LearningLantern.TextLesson/Migrations/20220704175320_Add ClassroomID.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearningLantern.TextLesson.Migrations
+{
+    public partial class AddClassroomID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
