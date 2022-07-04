6eb2dee0e26250984377f12266e814faeabcb661
ahmedezzatg <ahmed.ezzat.ga@gmail.com>
2022-07-04 15:03:11 +0200

    Update TextLesson Database

---
+++ TextLesson/LearningLantern.TextLesson/Migrations/20220704130231_v2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearningLantern.TextLesson.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "TextLessons",
+                newName: "Title");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "TextLessons",
+                newName: "Path");
+        }
+    }
+}
